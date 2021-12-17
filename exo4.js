eda56ed7d667ca64ecca78d4a8e039c22a2bcf8b
vmanachem <71695938+vmanachem@users.noreply.github.com>
2021-12-17 13:28:53 +0100

    Add fourth exercise

---
+++ exo4.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+
+    const queryObject = url.parse(req.url, true).query;
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.end('Your message :'+queryObject.message);
+});
+
+server.listen(3004, () => console.log('Le serveur tourne sur http://localhost:3004'));
